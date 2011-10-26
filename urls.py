09e9a9ecb84bdf239bf6cac4528c6f6c58183e38
Chris Missal <chris.missal@gmail.com>
2011-10-25 20:12:37 -0500

    Set up base layout and error pages

--- urls.py
+++ urls.py
@@ -6,15 +6,16 @@ from web.views import current_datetime
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns('icc.web.views',
     # Examples:
-    url(r'^$', 'icc.web.views.home', name='home'),
-    # url(r'^icc/', include('icc.foo.urls')),
-    url(r'^system-time/', 'icc.web.views.current_datetime', name='time'),
+    url(r'^$', 'home', name='home'),
+    url(r'^throw/', 'throw'),
+    url(r'^system-time/', 'current_datetime', name='time'),
+)
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+urlpatterns += patterns(
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+handler404 = 'icc.web.views.error'
--- web/views.py
+++ web/views.py
@@ -6,6 +6,12 @@ def home(request):
     speakers = [ { 'name': 'Chris Missal', 'session': 'jQuery Plugin Templates' }, { 'name': 'Zac Harlan', 'session': 'Data Correctness' } ]
     return render_to_response('home.html', { 'speakers': speakers })
 
+def error(request):
+    return render_to_response('errors/404.html')
+
+def throw(request):
+    return render_to_response('errors/500.html')
+
 def current_datetime(request):
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body></html>" % now
